61b11cd87328742a0edcc8158fccdc97131d016d
manamkiran <manamkiranyadav@gmail.com>
2020-07-14 21:58:41 +0530

    Updated Test Case

--- src/test/java/java8/util/ComparatorTest.java
+++ src/test/java/java8/util/ComparatorTest.java
@@ -8,18 +8,21 @@ import org.junit.Test;
 import java8.objects.Student;
 
 public class ComparatorTest {
-	
+
 	@Test
 	public void testComparator() {
-		
+
 		List<Student> list = new ArrayList<>();
-		list.add(new Student(34,"kiran","manam"));
-		list.add(new Student(34,"priya","manam"));
-		list.add(new Student(2,"vihaan","manam"));
-		Comparator<Student> studentComparator= Comparator.comparator(Student::getAge).thenComparingWith(Student::getFirstName).thenComparingWith(Student::getLastName);;
+		list.add(new Student(34, "kiran", "manam"));
+		list.add(new Student(34, "priya", "manam"));
+		list.add(new Student(34, "priya", "banu"));
+		list.add(new Student(2, "vihaan", "manam"));
+		Comparator<Student> studentComparator = Comparator.comparator(Student::getAge)
+														  .thenComparingWith(Student::getFirstName)
+														  .thenComparingWith(Student::getLastName);
 		list.sort(studentComparator);
 		list.forEach(System.out::println);
-		
+
 	}
 
 }
