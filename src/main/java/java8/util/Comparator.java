27fdaaf21dfa5d18851d4145c16e154e6db0fe1f
manamkiran <manamkiranyadav@gmail.com>
2020-07-14 21:45:51 +0530

    Adding Generics warning fix

--- src/main/java/java8/util/Comparator.java
+++ src/main/java/java8/util/Comparator.java
@@ -2,16 +2,21 @@ package java8.util;
 
 import java.util.function.Function;
 
+@FunctionalInterface
 public interface Comparator<T> extends java.util.Comparator<T> {
 
 	int compare(T t1, T t2);
 
-	public static <U> Comparator<U> comparator(Function<U, Comparable> function) {
+	public static <U,V extends Comparable<V>> Comparator<U> comparator(Function<U,V> function) {
 		return (t1, t2) -> function.apply(t1).compareTo(function.apply(t2));
 	}
 
-	public default Comparator<T> thenComparingWith(Function<T, Comparable> function) {
-		return (t1,t2) -> compare(t1, t2)!=0 ? compare(t1, t2) : comparator(function).compare(t1, t2);
+	public default <V extends Comparable<V>>Comparator<T> thenComparingWith(Function<T, V> function) {
+		return thenComparingWith(comparator(function));
+	}
+	
+	public default Comparator<T> thenComparingWith(Comparator<T> cmp) {
+		return (t1,t2) -> compare(t1, t2)!=0 ? compare(t1, t2) : cmp.compare(t1, t2);
 	}
 	
 
